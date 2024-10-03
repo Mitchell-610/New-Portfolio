667748874bab644d3d2af6927c1fd275331449d5
Mitchell-610 <Mheit602@gmail.com>
2024-10-03 18:44:33 -0400

    Contact me ran so we have decated page.

--- src/components/pages/Contact.jsx
+++ src/components/pages/Contact.jsx
@@ -6,6 +6,34 @@ import './Contact.css';
 
 export default function Contact() {
     return (
+        <div id="Contact-Me" className="Contact-Me container">
+            <h1>Contact Me</h1>
 
+            <div className="ref">
+                <div className="contact-item">
+                    <a href="https://github.com/Mitchell-610">
+                    <img src={git}></img>
+                    </a>
+                    <p>Github</p>
+                </div>
+
+                <div className="contact-item">
+                    <a href="https://www.linkedin.com/in/mitchell-heit-838ab4321/">
+                        <img src={linkdin} alt="LinkedIn" />
+                    </a>
+                    <p>LinkedIn</p>
+                </div>
+
+                <div className="contact-item">
+                    <a href="https://stackoverflow.com/users/27612998/mitchell-610">
+                        <img src={stack} alt="Stack OverFlow" />
+                    </a>
+                    <p>Stack Overflow</p>
+                </div>
+
+            </div>
+            <div className="contact-form">
+            </div>
+        </div>
     );
 }
